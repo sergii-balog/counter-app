10ed6e21892584cce7453a8b321474e18cd7224c
sbalog <sbalog@lanteria.net>
2019-06-25 19:08:01 +0300

    counters component added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: this.props.value,
+    product: this.props.product
   };
 
   //   constructor() {
@@ -20,26 +20,31 @@ class Counter extends Component {
   };
   render() {
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={this.handleIncrement}
-          className="btn btn-sm btn-secondary m-2"
-        >
-          +
-        </button>
-        <button
-          onClick={this.handleDecrement}
-          className="btn btn-sm btn-secondary"
-        >
-          -
-        </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      <div className="row">
+        <div className="col-sm-3">
+          <span className="m-2">
+            {this.props.children}
+            {this.state.product}
+          </span>
+        </div>
+        <div className="col-sm-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={this.handleIncrement}
+            className="btn btn-sm btn-secondary m-2"
+          >
+            +
+          </button>
+          <button
+            onClick={this.handleDecrement}
+            className="btn btn-sm btn-secondary"
+          >
+            -
+          </button>
+        </div>
+      </div>
     );
   }
 
